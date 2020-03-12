346724eda2d2529aec3213d5f6031bb63cb1f370
DNSR <good800322@gmail.com>
2020-03-12 14:00:47 +0800

    Install body-parser

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 300
 const exphb = require('express-handlebars')
 const mongoose = require('mongoose')
 const Record = require('./models/expenseTracker.js')
+const bodyParser = require('body-parser')
 
 mongoose.connect('mongodb://localhost/expenseTracker', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -21,13 +22,21 @@ db.once('open', () => {
 app.engine('handlebars', exphb({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//bodyParser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
   Record.find()
     .lean()
     .exec((err, records) => {
       if (err) console.error(err)
-      return res.render('index', { records: records })
+      //計算總額
+      let sum = 0
+      records.forEach(record => {
+        sum += record.amount
+      })
+      return res.render('index', { records: records, sum: sum })
     })
 })
 //use routes for /expense
