5398db1df6232b005515bfde2160b7b0b646d4ef
DNSR <good800322@gmail.com>
2020-03-12 14:56:03 +0800

    Add creation feature

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../models/expenseTracker.js')
 
 router.get('/', (req, res) => {
   res.redirect('/')
@@ -9,15 +10,40 @@ router.get('/new', (req, res) => {
   res.render('new')
 })
 //新增
-router.post('/', (req, res) => {
-  res.res('creation!')
+router.post('/new', (req, res) => {
+  let { name, date, category, amount } = req.body
+  amount = parseInt(amount)
+  Record.findOne({ name: name, date: date, amount: amount, })
+    .then(record => {
+      //排除重複輸入（資料完全一樣）------????????warning??????
+      if (record) {
+        return res.render('new', {
+          name,
+          date,
+          category,
+          amount
+        })
+      } else {
+        const newRecord = new Record({
+          name,
+          date,
+          category,
+          amount
+        })
+        newRecord.save()
+          .then(record => {
+            return res.redirect('/')
+          })
+          .catch(err => console.log(err))
+      }
+    })
 })
 //修改頁面
 router.get('/edit/:id', (req, res) => {
   res.render('update')
 })
 //修改
-router.post('/edit/:d', (req, res) => {
+router.post('/edit/:id', (req, res) => {
   res.res('update!')
 })
 //刪除
